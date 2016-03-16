9139b863b5ace9e435021596bbdce7c91f6fab68
Matthew Castleberry <mattcastle38@gmail.com>
2016-03-16 16:24:56 -0500

    Changed box to bullet notation

--- scripts/github.js
+++ scripts/github.js
@@ -7,7 +7,7 @@ jq142(document).ready(function() {
             for( i in result ) {
                 if (result[i].size > 0) {
                     jq142("#repo_list").append(
-                        "<div class="box"><h3>" + result[i].name + "</h3><hr noshade/><p>" +result[i].description + "<a href='" + result[i].html_url + "' target='_blank'>Visit Github</a></p></div>"
+                        "<li class="box"><h3>" + result[i].name + "</h3><hr noshade/>" +result[i].description + "<a href='" + result[i].html_url + "' target='_blank'>Visit Github</a></li>"
                     );
                 }
             }
