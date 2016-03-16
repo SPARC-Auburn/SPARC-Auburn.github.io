18fc700d74ec4ec3cdb45b62b257536b985134a8
Matthew Castleberry <mattcastle38@gmail.com>
2016-03-16 18:44:03 -0500

    added photo capability

--- scripts/github.js
+++ scripts/github.js
@@ -4,12 +4,10 @@ jq142(document).ready(function() {
         url: "https://api.github.com/users/SPARC-Auburn/repos",
         dataType: "json",
         success: function(result) {
-            for( i in result ) {
+            for(i in result) {
                 if (result[i].size > 0) {
                     jq142("#repo_list").append(
-                        "<li class='box'><h3>" + result[i].name + "</h3><hr noshade/>" +result[i].description + "<a href='" + result[i].html_url + "' target='_blank'>Visit Github</a></li>"
-                       //"<li class='box'><a href='" + result[i].html_url + "' target='_blank'>" +
-                    //result[i].name + "</a>: "+result[i].description + "</li>"
+                        "<li class='box'><h3>" + result[i].name + "</h3><hr noshade/><img src='" + result[i].html_url + "websitephoto.JPG' style='width:100%;' alt=''/>" +result[i].description + "<a href='" + result[i].html_url + "' target='_blank'>Visit Github</a></li>"
 
                     );
                 }
