cc8ade58c20d6e14ee551a8de5c0254a719855f7
Matthew Castleberry <mattcastle38@gmail.com>
2016-03-16 16:01:52 -0500

    added size check

--- scripts/github.js
+++ scripts/github.js
@@ -5,10 +5,12 @@ jq142(document).ready(function() {
         dataType: "json",
         success: function(result) {
             for( i in result ) {
-                jq142("#repo_list").append(
-                    "<li><a href='" + result[i].html_url + "' target='_blank'>" +
-                    result[i].name + "</a>: "+result[i].description + "</li>"
-                );
+                if (result[i].size > 0) {
+                    jq142("#repo_list").append(
+                        "<li><a href='" + result[i].html_url + "' target='_blank'>" +
+                        result[i].name + "</a>: "+result[i].description + "</li>"
+                    );
+                }
             }
             
         }
