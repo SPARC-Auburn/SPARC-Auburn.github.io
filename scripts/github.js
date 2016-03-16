5bfb7ddd6f890899ca89bb256ad515785e58176f
Matthew Castleberry <mattcastle38@gmail.com>
2016-03-16 16:30:09 -0500

    Changed li to class box

--- scripts/github.js
+++ scripts/github.js
@@ -8,7 +8,7 @@ jq142(document).ready(function() {
                 if (result[i].size > 0) {
                     jq142("#repo_list").append(
                         //"<li class="box"><h3>" + result[i].name + "</h3><hr noshade/>" +result[i].description + "<a href='" + result[i].html_url + "' target='_blank'>Visit Github</a></li>"
-                       "<li><a href='" + result[i].html_url + "' target='_blank'>" +
+                       "<li class='box'><a href='" + result[i].html_url + "' target='_blank'>" +
                     result[i].name + "</a>: "+result[i].description + "</li>"
 
                     );
