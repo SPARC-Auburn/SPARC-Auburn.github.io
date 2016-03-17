cdea348e0c38b886c983cf498d3c52e951c0494e
Matthew Castleberry <mattcastle38@gmail.com>
2016-03-16 21:11:50 -0500

    added picture checker

--- scripts/github.js
+++ scripts/github.js
@@ -7,10 +7,17 @@ jq142(document).ready(function() {
             for(i in result) {
                 var checkUrl = "https://raw.githubusercontent.com/SPARC-Auburn/" + result[i].name + "/master/addrepositorytowebsite.txt"; 
                 if (result[i].size > 0 && UrlExists(checkUrl)) {
-                    jq142("#repo_list").append(
-                        "<li class='box'><h3>" + result[i].name + "</h3><hr noshade/><img src='https://raw.githubusercontent.com/SPARC-Auburn/" + result[i].name + "/master/websitephoto.jpg' style='width:100%;' alt=''/>" +result[i].description + "<a href='" + result[i].html_url + "' target='_blank'><br/>View on Github</a></li>"
-
-                    );
+                    var checkPic = "https://raw.githubusercontent.com/SPARC-Auburn/" + result[i].name + "/master/websitephoto.jpg"; 
+                    if (UrlExists(checkPic)) {
+                        jq142("#repo_list").append(
+                            "<li class='box'><h3>" + result[i].name + "</h3><hr noshade/><img src='https://raw.githubusercontent.com/SPARC-Auburn/" + result[i].name + "/master/websitephoto.jpg' style='width:100%;' alt=''/>" +result[i].description + "<a href='" + result[i].html_url + "' target='_blank'><br/>View on Github</a></li>"
+                        );
+                    }
+                    else {
+                       jq142("#repo_list").append(
+                            "<li class='box'><h3>" + result[i].name + "</h3><hr noshade/>" +result[i].description + "<a href='" + result[i].html_url + "' target='_blank'><br/>View on Github</a></li>"
+                        ); 
+                    }
                 }
             }
             
