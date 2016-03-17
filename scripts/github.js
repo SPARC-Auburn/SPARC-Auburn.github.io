4ee588512ad5ec1a4935b08c3434745b2761e8c6
Matthew Castleberry <mattcastle38@gmail.com>
2016-03-16 20:31:57 -0500

    added picture checker

--- scripts/github.js
+++ scripts/github.js
@@ -5,7 +5,8 @@ jq142(document).ready(function() {
         dataType: "json",
         success: function(result) {
             for(i in result) {
-                if (result[i].size > 0) {
+                var checkUrl = "https://raw.githubusercontent.com/SPARC-Auburn/" + result[i].name + "/master/websitephoto.jpg"; 
+                if (result[i].size > 0 && UrlExists(checkUrl)) {
                     jq142("#repo_list").append(
                         "<li class='box'><h3>" + result[i].name + "</h3><hr noshade/><img src='https://raw.githubusercontent.com/SPARC-Auburn/" + result[i].name + "/master/websitephoto.jpg' style='width:100%;' alt=''/>" +result[i].description + "<a href='" + result[i].html_url + "' target='_blank'><br/>View on Github</a></li>"
 
@@ -16,3 +17,10 @@ jq142(document).ready(function() {
         }
     });
 });
+function UrlExists(url)
+{
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status!=404;
+}
