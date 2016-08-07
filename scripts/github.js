1c593b477089d680181473300992cf43649b4b34
mcberry23 <mattcastle38@gmail.com>
2016-08-07 17:08:24 -0500

    testing github code

--- scripts/github.js
+++ scripts/github.js
@@ -5,9 +5,9 @@ jq142(document).ready(function() {
         dataType: "json",
         success: function(result) {
             for(i in result) {
-                var checkUrl = "https://raw.githubusercontent.com/SPARC-Auburn/" + result[i].name + "/master/addrepositorytowebsite.txt"; 
+                var checkUrl = "https://raw.githubusercontent.com/SPARC-Auburn/" + result[i].name + "/master/addrepositorytowebsite.txt";
                 if (result[i].size > 0 && UrlExists(checkUrl)) {
-                    var checkPic = "https://raw.githubusercontent.com/SPARC-Auburn/" + result[i].name + "/master/websitephoto.jpg"; 
+                    var checkPic = "https://raw.githubusercontent.com/SPARC-Auburn/" + result[i].name + "/master/websitephoto.jpg";
                     if (UrlExists(checkPic)) {
                         jq142("#repo_list").append(
                             "<li class='box'><h3>" + result[i].name + "</h3><hr noshade/><img src='https://raw.githubusercontent.com/SPARC-Auburn/" + result[i].name + "/master/websitephoto.jpg' style='width:100%;' alt=''/>" +result[i].description + "<a href='" + result[i].html_url + "' target='_blank'><br/>View on Github</a></li>"
@@ -15,12 +15,12 @@ jq142(document).ready(function() {
                     }
                     else {
                        jq142("#repo_list").append(
-                            "<li class='box'><h3>" + result[i].name + "</h3><hr noshade/>" +result[i].description + "<a href='" + result[i].html_url + "' target='_blank'><br/>View on Github</a></li>"
-                        ); 
+                            "<li class='box'><h3>" + result[i].name + "</h3><hr noshade/>" +result[i].description + result[i].name + "<a href='" + result[i].html_url + "' target='_blank'><br/>View on Github</a></li>"
+                        );
                     }
                 }
             }
-            
+
         }
     });
 });
